c244a979498bf4ebc9df925a57a45dcc6668e553
KeDuCa96 <stevanduran96@gmail.com>
2022-07-11 20:23:57 -0500

    registrando URL y sus metodos

--- Router.php
+++ Router.php
@@ -2,7 +2,29 @@
 namespace MVC;
 
 Class Router {
-    public function __construct(){
-        echo 'router';
+
+    public $rutasGET =[];
+    public $rutasPOST =[];
+
+     public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+        
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        //debuguear($urlActual);
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if($fn){
+            call_user_func($fn, $this);
+        }else{
+            echo 'ERROR 404';
+        }
+        
     }
 }
--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router();
+
+$router->get('/admin', 'funcion_admin');
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/blog', 'funcion_blog');
+
+
+$router->comprobarRutas();
