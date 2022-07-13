84319d9f26a94772dd53b588b94730dd19775a49
KeDuCa96 <stevanduran96@gmail.com>
2022-07-13 00:56:55 -0500

    Actualizando en la BD

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -9,7 +9,8 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController
 {
-    public static function index(Router $router){ // Pasamos el objeto router para no perder referencias.
+    public static function index(Router $router)
+    { // Pasamos el objeto router para no perder referencias.
         $propiedades = Propiedad::all();
 
         $mensaje = $_GET['mensaje'] ?? null;
@@ -20,7 +21,8 @@ class PropiedadController
         ]);
     }
 
-    public static function create(Router $router){
+    public static function create(Router $router)
+    {
 
         $propiedad = new Propiedad;
         $vendedor = Vendedores::all();
@@ -59,14 +61,42 @@ class PropiedadController
         ]);
     }
 
-    public static function update(Router $router){
+    public static function update(Router $router)
+    {
         $id = validarIDurl('/admin');
 
         $propiedad = Propiedad::find($id);
         $vendedor = Vendedores::all();
         $errores = Propiedad::getErrores();
 
-        $router->render('/propiedades/actualizar',[
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //Nueva instancia
+            $propiedad = new Propiedad($_POST['propiedad']);
+            //Hasheamos la imagen
+            $nombreImagen = md5(uniqid(rand(), true)) . " .jpg";
+            //Setear la imagen
+            if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                // resize(cortar y dar nuevo tamaño) a la imagen con intervention
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600); 
+                $propiedad->setImagen($nombreImagen); // le pasamos el nombre el nombre al atributo de imagen
+            }
+
+            //Validar
+            $errores = $propiedad->validar();
+            //Valiamos que no tengamos errores
+            if (empty($errores)) {
+                //creamos caperta para subir imagenes
+                if (!is_dir(CARPETA_IMAGENES)) {
+                    mkdir(CARPETA_IMAGENES);
+                }
+                //Guardar imagen en servidor
+                $image->save(CARPETA_IMAGENES . $nombreImagen); // save es funcion de intervention
+                //Guardar base de datos
+                $propiedad->guardar();
+            }
+        }
+
+        $router->render('/propiedades/actualizar', [
             'propiedad' => $propiedad,
             'vendedores' => $vendedor,
             'errores' => $errores
